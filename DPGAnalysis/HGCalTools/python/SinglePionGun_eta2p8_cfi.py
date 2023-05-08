2ae7dd48612a3edef1fb20f7695f886de34ec9ca
Pedro <psilva@cern.ch>
2023-05-08 11:36:37 +0200

    adding particle gun

---
+++ DPGAnalysis/HGCalTools/python/SinglePionGun_eta2p8_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+# generate single nu_mu events
+generator = cms.EDProducer("FlatRandomEGunProducer",
+                           AddAntiParticle = cms.bool(True),
+                           PGunParameters = cms.PSet(
+                               MinEta = cms.double(2.799),
+                               MaxEta = cms.double(2.801),
+                               MinE = cms.double(1),
+                               MaxE = cms.double(120),
+                               MaxPhi = cms.double(3.14159265359),
+                               MinPhi = cms.double(-3.14159265359),
+                               PartID = cms.vint32(211)
+                           ),
+                           Verbosity = cms.untracked.int32(0),
+                           firstRun = cms.untracked.uint32(1),
+                           psethack = cms.string('multiple K-longs flat pT at eta=2.8')
+)
